a2d92f90dc2534d985cb267288a9ae2d11c69139
Patrick Huang <patrickhuang94@gmail.com>
2018-06-05 23:41:38 -0700

    Content for login modal

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -3,15 +3,31 @@ import {connect} from 'react-redux'
 import {bindActionCreators} from 'redux'
 
 import Modal from '../../elements/modal'
+import InputText from '../../elements/inputText'
+import Button from '../../elements/button'
 import {hideModal} from '../../actions'
 
 const styles = {
   modalContent: {
     display: 'flex',
-    marginLeft: 120
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    maxWidth: 400,
+    margin: '0 auto'
   },
   signInText: {
-    fontSize: 18
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textField: {
+    marginBottom: 25,
+    minWidth: 200
+  },
+  buttonStyle: {
+    paddingLeft: 0,
+    paddingRight: 0,
+    width: '100%'
   }
 }
 
@@ -22,12 +38,35 @@ class Login extends React.Component {
 		this.props.hideModal('login')
 	}
 
+  renderInputFields () {
+
+    return (
+      <div>
+        <InputText
+          label="Email"
+          style={styles.textField}
+        />
+        <InputText
+          label="Password"
+          style={styles.textField}
+        />
+      </div>
+    )
+  }
+
   render () {
 
     return (
       <Modal handleClose={this.handleCloseModal}>
         <div style={styles.modalContent}>
           <div style={styles.signInText}>Sign In</div>
+          {this.renderInputFields()}
+          <Button
+            uppercase
+            title="login"
+            size="medium"
+            style={styles.buttonStyle}
+          />
         </div>
       </Modal>
     )
