154256d30a5805b3872a506998119a57dc567bf3
Patrick Huang <patrickhuang94@gmail.com>
2018-07-13 01:15:32 -0700

    Calling it a night

--- src/server/routes/api/index.js
+++ src/server/routes/api/index.js
@@ -2,8 +2,6 @@ const express = require('express')
 const request = require('request')
 
 const router = express.Router()
-
-// const REDDIT_CLIENT_ID = 'LeltpchMUToy9w' // TODO: Put in .env
 // const REDIRECT_URI = 'https://patrickhuang94.github.io/react-reddit/'
 // const REDIRECT_URI = 'http://localhost:3000'
 // const REDDIT_AUTHORIZE_URL = `https://www.reddit.com/api/v1/authorize?client_id=${REDDIT_CLIENT_ID}&response_type="code"&state="random_string_here"&redirect_uri=${REDIRECT_URI}&redirect_uri=permanent&scope="identity"`
