ed12275787e4946645df55d2f737b0ca48575d6b
Patrick Huang <patrickhuang94@gmail.com>
2018-07-15 20:35:46 -0700

    Use dotenv on client side

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route } from 'react-router'
-import { Provider } from 'react-redux'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {Provider} from 'react-redux'
 
 import App from './components/App'
 import Home from './components/home'
 import OAuthRedirect from './components/oauthRedirect'
 import store, { history } from './store'
-
 import registerServiceWorker from './registerServiceWorker'
 
 import './index.css'
+
+// .env configs
+require('dotenv').config()
 
 const app = (
 	<Provider store={store}>
