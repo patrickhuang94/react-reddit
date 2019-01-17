0a3cc99ac3648ef5b0ecd5251a67b1ee26604d47
Patrick Huang <patrickhuang94@gmail.com>
2019-01-16 22:56:33 -0800

    Add more colors

--- src/colors.js
+++ src/colors.js
@@ -6,7 +6,10 @@ const colors = {
   darkGray: '#4E525A',
   darkestGray: '#35363B',
   darkBlue: '#042E50',
-  blue: '#5B76FB'
+  blue: '#5B76FB',
+  red: '#FF0000',
+  purple: '#8F75E3',
+  black: '#000000'
 }
 
 export default colors
